cd310c275d26bc8fb71d7fd85a570f01bc01a7bf
Fabian Schmid <fs@studer-raimann.ch>
2021-05-10 20:38:08 +0200

    v6

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'phbe_multiref';
 $version = '0.0.1';
-$ilias_min_version = '5.2.000';
-$ilias_max_version = '5.4.999';
+$ilias_min_version = '6.0';
+$ilias_max_version = '6.99';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'tt@studer-raimann.ch';
 ?>
